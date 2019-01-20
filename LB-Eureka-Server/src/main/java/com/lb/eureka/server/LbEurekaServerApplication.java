d247c3ab72d13080284fd8e0ddceff7d061aa54f
sarwarmukarram <mukarramsarwer@gmail.com>
2019-01-20 22:14:33 +0530

    first commit Eureka

---
+++ LB-Eureka-Server/src/main/java/com/lb/eureka/server/LbEurekaServerApplication.java
@@ -0,0 +1,14 @@
+package com.lb.eureka.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class LbEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LbEurekaServerApplication.class, args);
+	}
+}
